4460e92837ae41068fc5ab31b71a07ec260ce01a
Abdullah-Rizky-Adam-Safero <abdrizkyfero@gmail.com>
2024-03-11 17:12:41 +0700

    Update App.jsx

--- BajuBuddies/week5/src/App.jsx
+++ BajuBuddies/week5/src/App.jsx
@@ -1,15 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './style.css';
 import MainData from "./pages/mainHome";
 import AddProduct from "./pages/add";
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
     <AddProduct />
     <MainData/>      
       {/* 
        */}
-    </>
+    </BrowserRouter>
   );
 }
 
